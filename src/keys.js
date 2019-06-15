792c982040a1c043d9bec71ac45f98bb46e656cd
PavelMacias <47857266+PavelMacias@users.noreply.github.com>
2019-06-14 23:51:16 -0500

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,11 @@
 module.exports = {
     
     database: {
-        host: 'localhost',
-        user: 'root',
-        port: '3307',
-        database: 'nodeudas',
+        host: 'us-cdbr-iron-east-02.cleardb.net',
+        user: 'ba372fb5826eb1',
+        password:'3fb8a4d1',
+        port: '3308',
+        database: 'heroku_42e044772db6a2a',
     }
 }
+//mysql://ba372fb5826eb1:3fb8a4d1@us-cdbr-iron-east-02.cleardb.net/heroku_42e044772db6a2a?reconnect=true
