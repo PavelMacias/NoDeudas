cb20dc15ad541f351106911ae5a9698a654bb341
PavelMacias <47857266+PavelMacias@users.noreply.github.com>
2019-06-15 03:07:02 -0500

    update keys

--- src/keys.js
+++ src/keys.js
@@ -1,10 +1,10 @@
 module.exports = {
     
     database: {
-        host: 'us-cdbr-iron-east-02.cleardb.net',
-        user: 'ba372fb5826eb1',
-        password:'3fb8a4d1',
-        database: 'heroku_42e044772db6a2a'
+        host: 'db4free.net',
+        user: 'pavelmacias',
+        password:'123456789',
+        database: 'nodeudasmysql'
     }
 }
 //mysql://ba372fb5826eb1:3fb8a4d1@us-cdbr-iron-east-02.cleardb.net/heroku_42e044772db6a2a?reconnect=true
